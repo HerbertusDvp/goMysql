2bedcfb2c9c16a58e91133b274211473ea60ae1c
herbert <Herbertus@gmail.com>
2025-02-17 21:21:34 -0600

    Feat: menu, opcion 1 y 2

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,15 +1,20 @@
 package handlers
 
 import (
+	"bufio"
 	"fmt"
 	"goMysql/conect"
 	"goMysql/modelos"
 	"log"
+	"os"
+	"strconv"
 )
 
 var pln = fmt.Println
+var p = fmt.Print
 
 func Listar() {
+
 	conect.Conecta()
 	query := "select*from cliente"
 
@@ -36,6 +41,7 @@ func Listar() {
 
 func ListaById(id int) {
 
+	resultado := true
 	conect.Conecta()
 	query := "select * from cliente where id = ?"
 	datos, err := conect.Conexion.Query(query, id)
@@ -48,6 +54,7 @@ func ListaById(id int) {
 	clientes := modelos.Clientes{}
 
 	for datos.Next() {
+		resultado = false
 		var dato modelos.Cliente
 		err := datos.Scan(&dato.Id, &dato.Nombre, &dato.Correo, &dato.Telefono)
 		clientes = append(clientes, dato)
@@ -61,4 +68,76 @@ func ListaById(id int) {
 		}
 	}
 
+	if resultado {
+		pln("No se encontraron coinsidencias..")
+	}
+}
+
+func Insertar(cliente modelos.Cliente) {
+	conect.Conecta()
+	query := "insert into cliente values (null, ?, ?, ?)"
+	_, err := conect.Conexion.Exec(query, cliente.Nombre, cliente.Correo, cliente.Telefono)
+
+	if err != nil {
+		panic(err)
+	}
+	//pln(result)
+	pln("Se registró el cliente: ", cliente)
+}
+
+func Actualizar(cliente modelos.Cliente, id int) {
+	conect.Conecta()
+	query := "update cliente set nombre = ?, correo=?, telefono=? where id = ?"
+	_, err := conect.Conexion.Exec(query, cliente.Nombre, cliente.Correo, cliente.Telefono, id)
+
+	if err != nil {
+		panic(err)
+	}
+	//pln(result)
+	pln("Registro modificado")
+}
+
+func Eliminar(id int) {
+	conect.Conecta()
+	query := "delete from cliente where id = ?"
+	_, err := conect.Conexion.Exec(query, id)
+
+	if err != nil {
+		panic(err)
+	}
+	pln("Se elminó el cliente con id: ", id)
+}
+
+var id int
+var nombre, correo, telefono string
+
+func Menu() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	pln("-- Menú de opiones --\n")
+	pln("1. Listar clientes")
+	pln("2. Buscar cliente por id")
+	pln("3. Registrar cliente")
+	pln("4. Modificar cliente")
+	pln("5. Eliminar cliente")
+
+	if scanner.Scan() {
+		for {
+			if scanner.Text() == "1" {
+				Listar()
+				return
+			}
+			if scanner.Text() == "2" {
+				p("Ingresa el id del cliente: ")
+				if scanner.Scan() {
+					id, _ = strconv.Atoi(scanner.Text())
+				}
+				ListaById(id)
+				return
+			}
+			if scanner.Text() == "3" {
+
+			}
+		}
+	}
 }
--- main.go
+++ main.go
@@ -1,20 +1,38 @@
 package main
 
 import (
-	"fmt"
 	"goMysql/handlers"
 )
 
-var pln = fmt.Println
+//var pln = fmt.Println
 
 func main() {
-	//Todos los clientes
-	pln("Lista de todos los clientes: \n")
 
-	handlers.Listar()
+	handlers.Menu()
 
-	pln("\nListado de clientes por ID: \n")
+	/*
+		//Todos los clientes
+		pln("Lista de todos los clientes: \n")
 
-	handlers.ListaById(2)
+		handlers.Listar()
 
+		pln("\nListado de clientes por ID: \n")
+
+		handlers.ListaById(2)
+
+			pln("\nListado de clientes por ID: \n")
+
+			cliente := modelos.Cliente{Nombre: "Be Son Ju", Correo: "Son@gmail.com", Telefono: "5578822"}
+			handlers.Insertar(cliente)
+
+			cliente = modelos.Cliente{Nombre: "Moon", Correo: "moon@gmail.com", Telefono: "55778822"}
+			handlers.Actualizar(cliente, 4)
+
+		pln("Eliminar cliente con el id: ", 6)
+
+		handlers.Eliminar(6)
+
+		pln(" -- Listado -- ")
+		handlers.Listar()
+	*/
 }
